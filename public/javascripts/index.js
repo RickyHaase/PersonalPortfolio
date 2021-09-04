f186edc5f73d78d6dcce5279d63da0e844f587cc
R-Haase-CU <85689086+RickyHaase@users.noreply.github.com>
2021-09-04 18:45:30 -0400

    Revert "Fix for iOS viewport bug"
    This reverts commit 79b9c1dbff681def6aca6f0a0a3b7b6062512e57.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -42,10 +42,3 @@ function fadeIn(a, b){
     elementA.classList.remove('hidden');
     elementB.classList.remove('hidden');
 }
-
-// We listen to the resize event
-window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
